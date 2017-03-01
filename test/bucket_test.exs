f8bfd02e018e671fee8dd343e3f72157cc714fea
Mark Chandler <mark.allen.chandler@gmail.com>
2017-03-01 16:17:50 -0500

    Create basic Bucket module

---
+++ test/bucket_test.exs
@@ -0,0 +1,11 @@
+defmodule BucketTest do
+  use ExUnit.Case, async: true
+
+  test "stores values by key" do
+    {:ok, bucket} = KV.Bucket.start_link
+    assert KV.Bucket.get(bucket, "milk") == nil
+
+    KV.Bucket.put(bucket, "milk", 1)
+    assert KV.Bucket.get(bucket, "milk") == 1
+  end
+end
