f9a6fca614c39c1ba8a640732a4e25f6620dd1ed
Mark Chandler <mark.allen.chandler@gmail.com>
2017-03-01 16:21:06 -0500

    Add delete method to Bucket

--- test/bucket_test.exs
+++ test/bucket_test.exs
@@ -1,11 +1,23 @@
 defmodule BucketTest do
   use ExUnit.Case, async: true
 
-  test "stores values by key" do
+  setup do
     {:ok, bucket} = KV.Bucket.start_link
+    {:ok, bucket: bucket}
+  end
+
+  test "stores values by key", %{bucket: bucket} do
     assert KV.Bucket.get(bucket, "milk") == nil
 
     KV.Bucket.put(bucket, "milk", 1)
     assert KV.Bucket.get(bucket, "milk") == 1
+  end
+
+  test "keys can be deleted", %{bucket: bucket} do
+    KV.Bucket.put(bucket, "oranges", 10)
+    assert KV.Bucket.get(bucket, "oranges") == 10
+
+    KV.Bucket.delete(bucket, "oranges")
+    assert KV.Bucket.get(bucket, "oranges") == nil
   end
 end
